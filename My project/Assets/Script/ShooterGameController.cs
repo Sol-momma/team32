5eca84928a4d7490a4f96289d7c8273c940fc45d
Takumi-666 <108921895+Takumi-666@users.noreply.github.com>
2023-11-28 21:51:12 +0900

    スコアの表示

--- My project/Assets/Script/ShooterGameController.cs
+++ My project/Assets/Script/ShooterGameController.cs
@@ -1,17 +1,19 @@
 // クリックしたオブジェクトを消す処理（撃つ部分の処理）
 using UnityEngine;
+using TMPro; // TextMeshProを使用するために必要
 
 public class ShooterGameController : MonoBehaviour
 {
     public Camera playerCamera;
-    public Texture2D crosshairTexture; // 照準のテクスチャ
-    private Vector2 crosshairHotspot; // 照準の中心点
+    public Texture2D crosshairTexture;
+    private Vector2 crosshairHotspot;
     public int score = 0;
+    public TextMeshProUGUI scoreText; // この行を変更
 
     void Start()
     {
-        // 照準の中心を計算
-        crosshairHotspot = new Vector2(crosshairTexture.width / 2, crosshairTexture.height / 2);
+        // カーソルの設定など
+        UpdateScoreUI(); // 初期スコアをUIに表示
 
         // カーソルをカスタムの照準に変更
         Cursor.SetCursor(crosshairTexture, crosshairHotspot, CursorMode.Auto);
@@ -19,17 +21,30 @@ public class ShooterGameController : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) // マウス左クリックを検出
+        if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
             Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
 
             if (Physics.Raycast(ray, out hit))
             {
-                // ここでhit.transform.gameObjectがクリックされたオブジェクト
-                Destroy(hit.transform.gameObject); // オブジェクトを破壊
-                score++; // 得点を加算
+                Target target = hit.transform.GetComponent<Target>();
+                if (target != null)
+                {
+                    score += target.scoreValue;
+                }
+                else
+                {
+                    score++; // ターゲットコンポーネントがない場合は1点を加算
+                }
+                UpdateScoreUI(); // スコアUIを更新
+                Destroy(hit.transform.gameObject);
             }
         }
+    }
+
+    void UpdateScoreUI()
+    {
+        scoreText.text = "Score: " + score.ToString(); // スコアをテキストで表示
     }
 }
---
+++ My project/Assets/Script/Target.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public int scoreValue = 10; // このオブジェクトの点数。デフォルト値は10としています。
+}
