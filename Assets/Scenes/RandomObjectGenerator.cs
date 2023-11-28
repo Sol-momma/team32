b166c1212f02b88ebe82fa194981126d49df9943
Haru-330 <haruna.330romiyaneiro@gmail.com>
2023-11-28 13:39:44 +0900

    ランダム生成されたキューブをクリックすることで消す動作

--- Assets/Scenes/RandomObjectGenerator.cs
+++ Assets/Scenes/RandomObjectGenerator.cs
@@ -5,7 +5,8 @@ public class RandomObjectGenerator : MonoBehaviour
     public GameObject cubePrefab;
     public int numberOfObjects = 10;
     public float spawnRadius = 10f;
-    public float delayBetweenSpawns = 1f; // オブジェクト生成の間隔
+    public float delayBetweenSpawns = 1f;
+
     private int generatedObjectsCount = 0;
 
     void Start()
@@ -21,6 +22,13 @@ public class RandomObjectGenerator : MonoBehaviour
             randomPosition.y = 0;
 
             GameObject newCube = Instantiate(cubePrefab, randomPosition, Quaternion.identity);
+
+            // ClickToDeleteスクリプトをアタッチ
+            ClickToDelete clickToDeleteScript = newCube.AddComponent<ClickToDelete>();
+
+            // 生成されたキューブに対してClickToDeleteスクリプトがアタッチされる
+            clickToDeleteScript.enabled = true;
+
             generatedObjectsCount++;
         }
         else
