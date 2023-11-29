9ca2752fe6a0b8a9c46f1843e832a0f5d904be45
Haru-330 <haruna.330romiyaneiro@gmail.com>
2023-11-29 13:11:51 +0900

    新しくエフェクト追加

--- Assets/Scenes/RandomObjectGenerator.cs
+++ Assets/Scenes/RandomObjectGenerator.cs
@@ -6,6 +6,7 @@ public class RandomObjectGenerator : MonoBehaviour
     public int numberOfObjects = 10;
     public float spawnRadius = 10f;
     public float delayBetweenSpawns = 1f;
+    public float moveSpeed = 5f; // オブジェクトの移動速度
 
     private int generatedObjectsCount = 0;
 
@@ -22,6 +23,17 @@ public class RandomObjectGenerator : MonoBehaviour
             randomPosition.y = 0;
 
             GameObject newCube = Instantiate(cubePrefab, randomPosition, Quaternion.identity);
+
+            // Rigidbodyがアタッチされている前提
+            Rigidbody cubeRigidbody = newCube.GetComponent<Rigidbody>();
+            if (cubeRigidbody != null)
+            {
+                // 重力の影響を無効にする
+                cubeRigidbody.useGravity = false;
+
+                // Z方向に速度を設定
+                cubeRigidbody.velocity = Vector3.forward * moveSpeed;
+            }
 
             // ClickToDeleteスクリプトをアタッチ
             ClickToDelete clickToDeleteScript = newCube.AddComponent<ClickToDelete>();
