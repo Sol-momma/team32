e523d36c57bd71eed35f6ac46b786cba0d5d1b76
Takumi-666 <108921895+Takumi-666@users.noreply.github.com>
2023-12-02 21:56:35 +0900

    スコアの表示の修正

--- Assets/Scripts/PlayFabLeaderboardViewer.cs
+++ Assets/Scripts/PlayFabLeaderboardViewer.cs
@@ -32,7 +32,12 @@ public class PlayFabLeaderboardViewer : MonoBehaviour
             foreach (var item in result.Leaderboard)
             {
                 string displayName = item.DisplayName ?? "NoName";
-                nameRecordText.text += $"{item.Position + 1}位: {displayName} スコア {item.StatValue}\n";
+                // スコアを時、分、秒に分割
+                int hours = item.StatValue / 3600;
+                int minutes = (item.StatValue % 3600) / 60;
+                int seconds = item.StatValue % 60;
+                // 時、分、秒を結合して表示
+                nameRecordText.text += $"{item.Position + 1}位: {displayName} スコア {hours:D2}:{minutes:D2}:{seconds:D2}\n";
             }
         }, error =>
         {
