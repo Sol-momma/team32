4f8ad021dc49b7dc1d549011759a2f2aab1977d6
Takumi-666 <108921895+Takumi-666@users.noreply.github.com>
2023-12-02 13:39:21 +0900

    ランキングデータをコンソールログで表示

--- Assets/Scripts/PlayfabLogin.cs
+++ Assets/Scripts/PlayfabLogin.cs
@@ -1,8 +1,9 @@
+using System.Collections.Generic;
 using UnityEngine;
 using PlayFab.ClientModels;
 using PlayFab;
 
-public class PlayfabLogin : MonoBehaviour
+public class PlayFabLogin : MonoBehaviour
 {
     private void OnEnable()
     {
@@ -19,15 +20,57 @@ public class PlayfabLogin : MonoBehaviour
     private void PlayFabAuthService_OnLoginSuccess(LoginResult success)
     {
         Debug.Log("ログイン成功");
+        //SubmitScore(123);
+        GetLeaderboard();
     }
     private void PlayFabAuthService_OnPlayFabError(PlayFabError error)
     {
         Debug.Log("ログイン失敗");
-        Debug.Log(error.ToString());    // 原因など
     }
-
     void Start()
     {
         PlayFabAuthService.Instance.Authenticate(Authtypes.Silent);
+    }
+    public void SubmitScore(int playerScore)
+    {
+        PlayFabClientAPI.UpdatePlayerStatistics(new UpdatePlayerStatisticsRequest
+        {
+            Statistics = new List<StatisticUpdate>
+            {
+                new StatisticUpdate
+                {
+                    StatisticName = "SpeedScore",
+                    Value = playerScore
+                }
+            }
+        }, result =>
+        {
+            Debug.Log($"スコア {playerScore} 送信完了！");
+        }, error =>
+        {
+            Debug.Log(error.GenerateErrorReport());
+        });
+    }
+
+    public void GetLeaderboard()
+    {
+        PlayFabClientAPI.GetLeaderboard(new GetLeaderboardRequest
+        {
+            StatisticName = "SpeedScore"
+        }, result =>
+        {
+            foreach (var item in result.Leaderboard)
+            {
+                string displayName = item.DisplayName;
+                if (displayName == null)
+                {
+                    displayName = "NoName";
+                }
+                Debug.Log($"{item.Position + 1}位:{displayName} " + $"スコア {item.StatValue}");
+            }
+        }, error =>
+        {
+            Debug.Log(error.GenerateErrorReport());
+        });
     }
 }
