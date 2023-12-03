e05b6cfe5b24a40ecfba880e5a20acd65b1d86f9
Takumi-666 <108921895+Takumi-666@users.noreply.github.com>
2023-12-03 14:09:24 +0900

    ランキングの表示の修正、ユーザー名の文字制限

--- Assets/Scripts/PlayFabLeaderboardViewer.cs
+++ Assets/Scripts/PlayFabLeaderboardViewer.cs
@@ -2,15 +2,14 @@ using UnityEngine;
 using UnityEngine.UI;
 using PlayFab.ClientModels;
 using PlayFab;
-using System;
 
 public class PlayFabLeaderboardViewer : MonoBehaviour
 {
-    public Text nameRecordText; // 既存のフィールド
+    public Text rankingText; // ランキング表示用のテキスト
+    public Text scoreText;   // スコア表示用のテキスト
 
     void Start()
     {
-        // シーンがロードされたときにランキングを取得する
         GetLeaderboard();
     }
 
@@ -29,23 +28,27 @@ public class PlayFabLeaderboardViewer : MonoBehaviour
             StatisticName = "SpeedScore"
         }, result =>
         {
-            nameRecordText.text = ""; // テキストを初期化
+            rankingText.text = "";
+            scoreText.text = "";
+
             foreach (var item in result.Leaderboard)
             {
                 string displayName = item.DisplayName ?? "NoName";
-                // スコアを-1倍して元に戻し、100で割る
+
+                string line = $"{item.Position + 1}位:{displayName}";
+                line = line.PadRight(20); // 20は適宜調整
+
                 float correctedScore = (float)item.StatValue * -1 / 100;
+                int hours = (int)correctedScore / 3600;
+                int minutes = ((int)correctedScore % 3600) / 60;
+                int seconds = (int)correctedScore % 60;
+                int milliseconds = Mathf.RoundToInt((correctedScore - (int)correctedScore) * 100);
 
-                // スコアを時、分、秒に分割
-                int hours = (int)correctedScore / 60;
-                int minutes = (int)correctedScore % 60;
-                float seconds = correctedScore % 1f;
+                string hoursText = hours > 0 ? $"{hours}時間" : "";
+                string scoreLine = $"{hoursText}{minutes:D2}分{seconds:D2}秒{milliseconds:D2}";
 
-                // ミリ秒を計算（秒の小数部分を100倍し、整数に変換）
-                int milliseconds = Mathf.RoundToInt(seconds * 100);
-
-                string hoursText = hours > 0 ? $"{hours}分" : "";
-                nameRecordText.text += $"{item.Position + 1}位: {displayName} スコア {hoursText}{minutes:D2}秒{milliseconds:D2}\n";
+                rankingText.text += $"{line}\n";
+                scoreText.text += $"スコア：{scoreLine}\n";
             }
         }, error =>
         {
--- Assets/Scripts/PlayFabScoreSubmitter.cs
+++ Assets/Scripts/PlayFabScoreSubmitter.cs
@@ -3,11 +3,19 @@ using UnityEngine.UI;
 using PlayFab.ClientModels;
 using PlayFab;
 using System.Collections.Generic;
-
 public class PlayFabScoreSubmitter : MonoBehaviour
 {
     public InputField nameInputField; // ユーザー名入力用のInputField
+    public Text errorMessageText;     // エラーメッセージ表示用のText
+    public SceneChanger sceneChanger; // SceneChangerへの参照
 
+
+    void Start()
+    {
+        // 初期化時にエラーメッセージを非表示に設定
+        errorMessageText.text = "";
+        errorMessageText.gameObject.SetActive(false); // 必要に応じて
+    }
     public void SubmitScoreWithName()
     {
         if (!GlobalLoginState.IsLoggedIn)
@@ -16,32 +24,39 @@ public class PlayFabScoreSubmitter : MonoBehaviour
             return;
         }
 
-        ResultScreen resultScreen = FindObjectOfType<ResultScreen>(); // ResultScreenを検索
-        float score = resultScreen.GetScore(); // ResultScreenからスコアを取得
+        if (nameInputField.text.Length > 6)
+        {
+            errorMessageText.text = "ユーザー名は6文字までなのだ";
+            errorMessageText.gameObject.SetActive(true); // エラーメッセージを表示
+            return;
+        }
+
+        // エラーがない場合はエラーメッセージを非表示に
+        errorMessageText.gameObject.SetActive(false);
+
+        ResultScreen resultScreen = FindObjectOfType<ResultScreen>();
+        float score = resultScreen.GetScore();
         Debug.Log(score);
 
-        // スコアを100倍にして整数に変換し、さらに-1を掛ける
         int scaledScore = Mathf.RoundToInt(score * 100) * -1;
 
         var request = new UpdatePlayerStatisticsRequest
         {
             Statistics = new List<StatisticUpdate>
-        {
-            new StatisticUpdate
             {
-                StatisticName = "SpeedScore",
-                Value = scaledScore // 負のスケーリングされたスコアを使用
+                new StatisticUpdate
+                {
+                    StatisticName = "SpeedScore",
+                    Value = scaledScore
+                }
             }
-        }
         };
 
-        PlayFabClientAPI.UpdatePlayerStatistics(request, OnStatisticsUpdate, OnPlayFabError);
-    }
-
-    private void OnStatisticsUpdate(UpdatePlayerStatisticsResult result)
-    {
-        Debug.Log("スコア登録成功");
-        SetDisplayName(); // 名前を設定
+        PlayFabClientAPI.UpdatePlayerStatistics(request, result =>
+        {
+            Debug.Log("スコア登録成功");
+            SetDisplayName();
+        }, OnPlayFabError);
     }
 
     private void SetDisplayName()
@@ -51,16 +66,15 @@ public class PlayFabScoreSubmitter : MonoBehaviour
             DisplayName = nameInputField.text
         };
 
-        PlayFabClientAPI.UpdateUserTitleDisplayName(request, OnDisplayNameUpdate, OnPlayFabError);
-    }
-
-    private void OnDisplayNameUpdate(UpdateUserTitleDisplayNameResult result)
-    {
-        Debug.Log("表示名を更新しました");
+        PlayFabClientAPI.UpdateUserTitleDisplayName(request, result =>
+        {
+            Debug.Log("表示名を更新しました");
+            sceneChanger.ChangeSceneToRanking(); // ここでシーン遷移をトリガー
+        }, OnPlayFabError);
     }
 
     private void OnPlayFabError(PlayFabError error)
     {
-        Debug.Log("PlayFabエラー: " + error.GenerateErrorReport());
+        Debug.LogError("PlayFabエラー: " + error.GenerateErrorReport());
     }
 }
